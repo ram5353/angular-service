fecf30c06cbfb243a74428c1cd339beef7f50b0b
ram5353 <ramkiran5353@gmail.com>
2020-04-08 00:08:56 +0530

    Accounts Service and Hierarichal Injector

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {LoggingService} from '../logging.service';
+import {AccountsService} from '../accounts.service';
+
 
 @Component({
   selector: 'app-account',
@@ -10,13 +12,12 @@ import {LoggingService} from '../logging.service';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService, private accountService: AccountsService) {}
 
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountService.statusUpdate(this.id, status);
     this.loggingService.logStatusChange(status);
   }
 }
